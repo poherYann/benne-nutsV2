3dffb728d52acdf95f56d81521a9e70e4e6beabc
Yann Poher <yan.poher@gmail.com>
2021-01-22 11:47:37 +0100

    json foreach

--- src/Controller/ToulouseController.php
+++ src/Controller/ToulouseController.php
@@ -19,24 +19,31 @@ class ToulouseController extends AbstractController
     {
         $response = $this->client->request(
             'GET',
-            'https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=points-dapport-volontaire-dechets-et-moyens-techniques&q=&facet=commune&facet=flux&facet=centre_ville&facet=prestataire&facet=zone&facet=pole'
+            'https://data.toulouse-metropole.fr/api/records/1.0/search/?dataset=points-dapport-volontaire-dechets-et-moyens-techniques&q=&rows=50&facet=commune&facet=flux&facet=centre_ville&facet=prestataire&facet=zone&facet=pole'
         );
         $content = $response->getContent();
         $decode = json_decode($content, true);
-        dump($decode);
-        dump($decode["records"][0]["fields"]);
 
-        $commune = $decode["records"][0]["fields"]["commune"];
-        $zone = $decode["records"][0]["fields"]["zone"];
-        $voie = $decode["records"][0]["fields"]["voie"];
-        $adresse = $decode["records"][0]["fields"]["adresse"];
-        $precizion = $decode["records"][0]["fields"]["precision"];
-        $numero = $decode["records"][0]["fields"]["numero"];
-        $type = $decode["records"][0]["fields"]["geo_shape"]["type"];
+
+      foreach ($decode["records"] as $fields) {
+            dump($fields);
+            foreach ( $fields["fields"] as $commune) {
+                dump($commune);
+            }
+      }
+
+         /*vérifier si la donnée existe ou pas*/
+        /*$commune = $decode["records"][0]["fields"]["commune"];
         $longitude = $decode["records"][0]["fields"]["geo_shape"]["coordinates"][0][1];
         $latitude = $decode["records"][0]["fields"]["geo_shape"]["coordinates"][0][0];
+        $zone = $decode["records"][0]["fields"]["zone"];
+        $voie = $decode["records"][0]["fields"]["voie"];
+        $precizion = $decode["records"][0]["fields"]["precision"];
+        $numero = $decode["records"][0]["fields"]["numero"];
+        $type = $decode["records"][0]["fields"]["geo_shape"]["type"];*/
 
-        dump(count($decode["records"]));
+
+
 
 
 
