c27fb829160e3cddcd55199bc217568643dc1725
Victor <lerouxvictor3@gmail.com>
2021-02-12 10:29:16 +0100

    Update du 11/02/2021

--- src/Controller/PoitiersController.php
+++ src/Controller/PoitiersController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Poitiers;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
+use App\DataFixtures\AppFixtures;
 
 class PoitiersController extends AbstractController
 {
@@ -19,20 +21,47 @@ class PoitiersController extends AbstractController
             'GET',
             'https://www.data.gouv.fr/fr/datasets/r/3f154ac8-f2a5-4788-8eba-0753dcc2390d'
         );
+
+
         $content = $response->getContent();
+
         $decode = json_decode($content, true);
-        for ($i = 0; $i <= count($decode["features"]) -1; $i++)
-            {
-                dump($decode["features"][$i]);
+
+        dump($decode);
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        for ($i = 0; $i <= count($decode["features"]) - 1; $i++) {
+            $key = $decode["features"][$i]["properties"];
+
+            if (isset($key["Adresse"]) && isset($key["Observation"]) && isset($decode["features"][$i]["geometry"]["coordinates"][1]) && isset($decode["features"][$i]["geometry"]["coordinates"][0])) {
+                $benne = new Poitiers();
+                $benne->setAdresse($key["Adresse"]);
+                $benne->setObservation($key["Observation"]);
+//              $benne->setAdresse($decode["features"][$i]["properties"]["Adresse"]);
+//              $benne->setObservation($decode["features"][$i]["properties"]["Observation"]);
+                $benne->setLatitude($decode["features"][$i]["geometry"]["coordinates"][1]);
+                $benne->setLongitude($decode["features"][$i]["geometry"]["coordinates"][0]);
+
+                $entityManager->persist($benne);
             }
+
+        }
+
+
+        $entityManager->flush();
+
+
         return $this->render('poitiers/index.html.twig', [
             'controller_name' => 'PoitiersController',
         ]);
     }
 
-    private $client;
+    private
+        $client;
 
-    public function __construct(HttpClientInterface $client)
+    public
+    function __construct(HttpClientInterface $client)
     {
         $this->client = $client;
     }
