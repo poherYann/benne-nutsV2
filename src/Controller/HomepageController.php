cf0f532407892863d7060c6d0edf36f258d0a6e5
guillaumeC76 <57393436+guillaumeC76@users.noreply.github.com>
2021-03-05 16:45:42 +0100

    Commit pour le front avec page d'accueil et page de conatct

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(): Response
+    {
+        return $this->render('poitiers/home.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+}
+
